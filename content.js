979eecaf01e26720e858e950977033d5e7dda8ae
Stefan Vojvodic <vojvodics>
2018-01-02 19:22:32 +0100

    added margin top and botom margin

--- content.js
+++ content.js
@@ -8,6 +8,10 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
             for (const image of images) {
                 if (image.src === img.src) {
                     image.style.transform = `rotate(${img.rotate}deg)`;
+                    // image.parentElement.style.height = 'auto';
+                    // TODO change margin depending on the angle of rotation
+                    image.style.marginTop = image.style.marginBottom = `${image.width/2}px`;
+                    // image.parentElement.style.overflow = 'auto';
                     break;
                 }
             }
